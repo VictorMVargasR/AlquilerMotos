a438947ff8e9112331a75d2f54fbe6a13b3c52d2
jaquevedo <113221675+jaquevedoUSA@users.noreply.github.com>
2022-10-11 16:12:11 -0500

    Score y Admin

---
+++ src/main/java/co/edu/usa/ejemplojpa/crud/controller/ScoreController.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.usa.ejemplojpa.crud.controller;
+
+import co.edu.usa.ejemplojpa.crud.models.Score;
+import co.edu.usa.ejemplojpa.crud.services.ScoreService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Victor
+ */
+@RestController
+@RequestMapping("api/Score")
+public class ScoreController {
+
+    @Autowired
+    private ScoreService ScoreService;
+
+    @GetMapping("/all")
+    public List<Score> getAll() {
+        return ScoreService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Score> getScore(@PathVariable("id") int ScoreId) {
+        return ScoreService.getScore(ScoreId);
+    }
+    
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Score save(@RequestBody Score c) {
+        return ScoreService.save(c);
+    }
+    
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Score update(@RequestBody Score c){
+        return ScoreService.update(c);
+    }
+    
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int ScoreId){
+        return ScoreService.delete(ScoreId);
+    }
+}
